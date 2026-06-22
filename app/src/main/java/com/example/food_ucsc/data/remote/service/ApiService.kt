4067493c328a17ce67fa3802d7b7db7880cb59c6
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2026-06-22 00:47:45 -0400

    Se agregan métodos GET de favoritos, consejos y desafíos

--- app/src/main/java/com/example/food_ucsc/data/remote/service/ApiService.kt
+++ app/src/main/java/com/example/food_ucsc/data/remote/service/ApiService.kt
@@ -1,7 +1,10 @@
 package com.example.food_ucsc.data.remote.service
 
+import com.example.food_ucsc.data.remote.dto.ChallengeDto
+import com.example.food_ucsc.data.remote.dto.FavoriteDto
 import com.example.food_ucsc.data.remote.dto.MenuDto
 import com.example.food_ucsc.data.remote.dto.RestaurantDto
+import com.example.food_ucsc.data.remote.dto.TipDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -14,4 +17,13 @@ interface ApiService {
 
     @GET("locales/{id}/menus")
     suspend fun getMenusByRestaurant(@Path("id") restaurantId: Int): List<MenuDto>
+
+    @GET("usuarios/{id}/favoritos")
+    suspend fun getFavoritesByUser(@Path("id") userId: Int): List<FavoriteDto>
+
+    @GET("consejos")
+    suspend fun getTips(): List<TipDto>
+
+    @GET("desafios")
+    suspend fun getChallenges(): List<ChallengeDto>
 }
