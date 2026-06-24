4a12451f750e2c50dbb76967231752b9b8038359
Neovix33 <vlillo@ing.ucsc.cl>
2026-06-24 10:33:08 -0400

    Solucion parcial para la solución de error de conexión la api

--- app/src/main/java/com/example/food_ucsc/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/food_ucsc/data/remote/RetrofitClient.kt
@@ -5,6 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
+
+    // Reemplazar http://10.0.2.2:8000/api/ por http://IPV4_DE_TU_PC:8000/api/
     private const val BASE_URL = "http://10.0.2.2:8000/api/"
 
     private val retrofit: Retrofit by lazy {
