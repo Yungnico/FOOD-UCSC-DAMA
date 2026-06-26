62b4e472cf61c6077eee039c718fb4f7d963bdd7
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2026-06-26 19:40:50 -0400

    Se arregla detalle

--- app/src/main/java/com/example/food_ucsc/di/NetworkModule.kt
+++ app/src/main/java/com/example/food_ucsc/di/NetworkModule.kt
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 
 object NetworkModule {
 
-    private const val BASE_URL = "http:///api/"
+    private const val BASE_URL = "http://10.0.2.2:8000/api/"
 
     fun provideAuthInterceptor(sessionManager: SessionManager): AuthInterceptor {
         return AuthInterceptor(sessionManager)
